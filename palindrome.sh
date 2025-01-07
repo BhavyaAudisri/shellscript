ec03cc1d2c30d76a8536983c171d8a90c85a0091
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-01-07 20:59:02 +0530

    changes done

--- palindrome.sh
+++ palindrome.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Enter a string: " 
-read -p string # Reverse the string and compare with the original
+read -s string # Reverse the string and compare with the original
 
 if [[ "$string" == "$(rev <<< "$string")" ]];
  then
-    echo "The entered string '$string' is a palindrome."
+    echo "The entered string $string is a palindrome."
 else
-    echo "The entered string '$string' is not a palindrome."
+    echo "The entered string $string is not a palindrome."
 fi
