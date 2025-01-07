47385b67bd44d5ca321652730ba3d3492f15b6eb
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-01-07 22:37:47 +0530

    changes done

--- wordcount.sh
+++ wordcount.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
  echo "total no of words in a file is :" #display total no of words in a file
  echo " $(wc -w sample.txt)"
+ echo "total no of line in a file"
+ echo " $(wc -l sample.txt)"
