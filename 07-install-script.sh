ec2568da4f00988a052c8fc528ea3b3e498514d6
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-01-21 18:15:33 +0530

    shell scripting

--- 07-install-script.sh
+++ 07-install-script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 userid=$(id -u)
-if [ $userid -ne 0]
+if [ $userid -ne 0 ]
 then    
     echo " you must have root access "
     exit 1
 fi
 dnf list installed mysql
 
-if [ $? -ne 0]
+if [ $? -ne 0 ]
 then    
     dnf install mysql -y
     if [ $? -ne 0]
