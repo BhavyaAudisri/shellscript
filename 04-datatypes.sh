9bd107faa4e5a796d06a16e0346050604d0a89ba
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-01-21 15:55:51 +0530

    datatypes

--- 04-datatypes.sh
+++ 04-datatypes.sh
@@ -2,9 +2,11 @@
 
 NUMBER1=$1
 NUMBER2=$2
+movies=("RRR" "Devara" "game changer")
 
 TIMESTAMP=$(date)
 echo "Script executed at: $TIMESTAMP"
 SUM=$(($NUMBER1+$NUMBER2))
 
 echo "SUM of $NUMBER1 and $NUMBER2 is: $SUM"
+echo "$movies"
