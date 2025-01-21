13408bda7bde183cb9688badd2fe97fb4297b21c
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-01-21 18:01:51 +0530

    shell scripting

---
+++ 07-install-script(exitstatus).sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+userid=$(id -u)
+if [ $userid -ne 0]
+then    
+    echo " you must have root access "
+    exit 1
+fi
+dnf list installed mysql
+
+if [ $? -ne 0]
+then    
+    dnf install mysql -y
+    if [ $? -ne 0]
+    then
+        echo " installing mysql... failure "
+        exit 1
+    else
+        echo " installing mysql .... success "
+    fi
+else
+    echo " mysql is already installed "
+fi
