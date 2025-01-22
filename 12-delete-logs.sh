4f4334470307517c1530bfd7ac13898e43df9b0a
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-01-22 14:11:12 +0530

    delete-logs

--- 12-delete-logs.sh
+++ 12-delete-logs.sh
@@ -38,6 +38,6 @@ echo "Files to be deleted: $FILES_TO_DELETE"
 
 while read -r file
 do
-    echo "Deleting file: $file" &>>$LOG_FILE_NAME
+    echo "Deleting file: $file"
     rm -rf $file
 done <<< $FILES_TO_DELETE
